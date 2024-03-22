d6d81febeeab341eb6358d8d7c01dd0f2e8d785a
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-22 16:09:59 +0530

    Update deployServer.sh

--- deployServer.sh
+++ deployServer.sh
@@ -1,6 +1,6 @@
 sudo apt update && sudo apt install apache2 -y
-sudo apt install nodejs
-sudo npm install firebase
+sudo apt install nodejs -y
+sudo npm install firebase -y
 sudo mv cafe /var/www/html
 
 cd /var/www/html
