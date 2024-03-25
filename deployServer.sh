378ea5413d66d93f6fa51c074dcd50472bc8e724
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-25 09:28:19 +0530

    Update deployServer.sh

--- deployServer.sh
+++ deployServer.sh
@@ -1,6 +1,7 @@
 sudo apt update && sudo apt install apache2 -y
-sudo apt install nodejs npm -y
-sudo npm install firebase -y
+# 20/3/2024 Added for Firebase AB Testing. But AB testing of web app using Remote Config not enabled yet in Firebase.
+# sudo apt install nodejs npm -y
+# sudo npm install firebase -y
 sudo mv cafe /var/www/html
 
 cd /var/www/html
